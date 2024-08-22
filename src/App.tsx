f87baa4f4f622998febbcc821ebc20a16e18b86e
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-22 12:24:01 -0400

    Add motion number

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import MotionNumber from 'motion-number'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  const handleIncrease = () => {
+    const random = Math.floor(Math.random() * 100) + 1;
+    setCount(count + random);
+  }
 
   return (
     <>
       <h1>Motion Number Demo</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <p style={{ fontSize: 50, marginTop: 10 }}>
+        <MotionNumber
+          value={count}
+          format={{ notation: 'compact' }} />
+      </p>
     </>
   )
 }
